b9044ac04fe002f56e6da0be26fa1c172a123cb3
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-05-29 11:09:52 -0300

    Inferencia dinamica de tipos com a palavra reservada var

---
+++ Exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class Inferencia {
+	public static void main(String[] args) {
+		/*
+		 * Inserido a partir do Java 10, onde o valor é utilizado para dar
+		 * o tipo da variavel.
+		 * */
+		var inferencia = 4.5;
+		
+	}
+}
