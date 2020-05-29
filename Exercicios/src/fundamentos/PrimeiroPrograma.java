cdd7d0b752f2aa0a2ae4f0c66e2a9a28c8cc2dc7
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-05-29 10:29:12 -0300

    Comentarios e javadoc

--- Exercicios/src/fundamentos/PrimeiroPrograma.java
+++ Exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,7 +1,24 @@
 package fundamentos;
 
+/**
+ * Javadoc
+ * @author kalil <Treinamento de Javadoc>
+ * @since 1.0
+ * @category <Treino>
+ *
+ */
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
+		// Primeiro comentário
 		System.out.println("Hello World.");
+		
+		/*
+		 * Comentario de bloco
+		 * Linha 01
+		 * Linha 02
+		 * 
+		 */
+		
+		//System.out.println("Uma linha de código comentada");
 	}
 }
