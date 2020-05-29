88bf9be90e52cbd38fe483c8c832e4877353d883
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-05-29 12:32:36 -0300

    Conversao de tipos

---
+++ Exercicios/src/fundamentos/ConversaoTipos.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class ConversaoTipos {
+	public static void main(String[] args) {
+		// Obs: Pode ocorrer perda de dados
+		double a = 1;
+		float b = (float) 1.646523232332;
+		int c = 130;
+		byte d = (byte) c;
+		double e = 1.98656;
+		int f = (int) e;
+	}
+}
