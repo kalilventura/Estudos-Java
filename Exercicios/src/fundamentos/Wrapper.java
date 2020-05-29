94021bacdaaad9cedbe7fe2703c39648ca9b171f
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-05-29 12:27:55 -0300

    wrappers

---
+++ Exercicios/src/fundamentos/Wrapper.java
@@ -0,0 +1,26 @@
+package fundamentos;
+
+public class Wrapper {
+	public static void main(String[] args) {
+		// Wrapper -> Tipos complexos para os tipos primitivos
+		// ele "envolve" os tipos primitivos
+		
+		//byte
+		Byte b = 100;		
+		//short
+		Short s = 1000;		
+		//int
+		Integer i = Integer.parseInt("10000");		
+		//long
+		Long l = 10000L;		
+		//boolean
+		Boolean bool = Boolean.parseBoolean("false");
+		//char
+		Character c = '#';
+		//float
+		Float f = 123.0F;
+		//double
+		Double d = 1234.5678;
+		
+	}
+}
