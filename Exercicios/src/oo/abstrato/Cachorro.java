6cb7b9f9c4eaffb8a723aa02465aa75a0e5c0ec1
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-01 16:11:33 -0300

    Abstracao

---
+++ Exercicios/src/oo/abstrato/Cachorro.java
@@ -0,0 +1,15 @@
+package oo.abstrato;
+
+public class Cachorro extends Mamifero {
+
+	@Override
+	public String mamar() {
+		return "Tomando leite";
+	}
+	
+	@Override
+	public String mover() {
+		return "Usando as patas";
+	}
+
+}
