a3b826c3411837417740120deeccbe33d8f1a2dd
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-01 09:58:14 -0300

    Conjunto Heterogenio com HashSet

--- Exercicios/src/classe/Usuario.java
+++ Exercicios/src/classe/Usuario.java
@@ -4,24 +4,17 @@ public class Usuario {
 
 	String nome;
 	String email;
-	
+
 	public boolean equals(Object objeto) {
-		
-		if(objeto instanceof Usuario) {
+		if (objeto instanceof Usuario) {
 			Usuario outro = (Usuario) objeto;
-			
-			
+
 			boolean nomeIgual = outro.nome.equals(this.nome);
 			boolean emailIgual = outro.email.equals(this.email);
-			
-			return nomeIgual && emailIgual;			
+
+			return nomeIgual && emailIgual;
 		} else {
 			return false;
 		}
-	}
-	
-	// O hascode será abordado em outra aula!
-	public int hashCode() {
-		return 0;
 	}
 }
