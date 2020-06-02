f30938d78def0ff5bd9b5dfa8f5bde3f55505e1b
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-02 18:13:16 -0300

    Externalizando os dados de conexao ao banco de dados

--- Exercicios/src/jdbc/FabricaConexao.java
+++ Exercicios/src/jdbc/FabricaConexao.java
@@ -1,24 +1,36 @@
 package jdbc;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {
 	public static Connection obterConexao() {
 		try {
+			Properties prop = getProperties();
 			
-			final String url = "jdbc:mysql://localhost:3306/curso_java?verifyServerCertificate=false&useSSL=true";
-			final String user = "root";
-			final String password = "root";
+			final String url = prop.getProperty("banco.url");
+			final String user = prop.getProperty("banco.usuario");
+			final String password = prop.getProperty("banco.senha");
 
 			System.out.println("Conexão efetuada com sucesso");
 
 			return DriverManager.getConnection(url, user, password);
 
-		} catch (SQLException e) {
+		} catch (SQLException | IOException e) {
 			System.out.println("Não foi possivel conectar ao banco de dados");
 			throw new RuntimeException(e);
 		}
+	}
+	
+	private static Properties getProperties() throws IOException {
+		Properties prop = new Properties();
+		
+		String path = "/conexao.properties";
+		
+		prop.load(FabricaConexao.class.getResourceAsStream(path));
+		return prop;
 	}
 }
