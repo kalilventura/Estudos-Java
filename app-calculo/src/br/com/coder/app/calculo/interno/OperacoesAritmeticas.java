c7119de6ff3ebe109728996431fdf3ab53384314
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-04 12:49:41 -0300

    Requires Transitive

--- app-calculo/src/br/com/coder/app/calculo/interno/OperacoesAritmeticas.java
+++ app-calculo/src/br/com/coder/app/calculo/interno/OperacoesAritmeticas.java
@@ -3,8 +3,11 @@ package br.com.coder.app.calculo.interno;
 import java.util.Arrays;
 import java.util.function.DoubleBinaryOperator;
 
+import br.com.coder.app.logging.Logger;
+
 public class OperacoesAritmeticas {
 	public double soma(double... numbers) {
+		Logger.info("Somando ...");
 		DoubleBinaryOperator sum = (t, a) -> t + a;
 		return Arrays.stream(numbers).reduce(0, sum);
 	}
--- app-calculo/src/module-info.java
+++ app-calculo/src/module-info.java
@@ -1,3 +1,6 @@
 module app.calculo {
 	exports br.com.coder.app.calculo;
+	// O transitive passa para os que chamarem esse modulo acessarem o logger caso algum 
+	// metodo de a referencia externalizada para ele
+	requires transitive app.logging;
 }
