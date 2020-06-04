94b0fe742002ade2ee42c43678a86208a9bb19cd
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-04 10:00:52 -0300

    Exemplo de Heranca para criar tabelas

---
+++ Exercicios-jpa/src/teste/heranca/NovoAluno.java
@@ -0,0 +1,18 @@
+package teste.heranca;
+
+import infra.DAO;
+import modelo.heranca.Aluno;
+import modelo.heranca.AlunoBolsista;
+
+public class NovoAluno {
+
+	public static void main(String[] args) {
+		DAO<Aluno> dao = new DAO<>();
+		Aluno aluno = new Aluno(123L, "João");
+		AlunoBolsista alunoBolsista = new AlunoBolsista(345L, "Maria", 1000);
+		
+		dao.atomicInsert(aluno);
+		dao.atomicInsert(alunoBolsista);
+	}
+
+}
