76284178b97ff913b010496b4aa149a5f93c7826
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-06-04 23:15:47 -0300

    Passando parametros para uma requisicao get

--- spring-boot/src/main/java/br/com/coder/springboot/controllers/ClienteController.java
+++ spring-boot/src/main/java/br/com/coder/springboot/controllers/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.coder.springboot.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.coder.springboot.models.Cliente;
@@ -9,9 +11,19 @@ import br.com.coder.springboot.models.Cliente;
 @RestController
 @RequestMapping(path = "/clientes")
 public class ClienteController {
-	
+
 	@GetMapping(path = "/qualquer")
 	public Cliente ObterCliente() {
 		return new Cliente(28, "Pedro", "123456789-00");
+	}
+
+	@GetMapping(path = "/{id}")
+	public Cliente obterClientePorId(@PathVariable int id) {
+		return new Cliente(id, "Maria", "987654321-00");
+	}
+
+	@GetMapping
+	public Cliente obterClientePorId2(@RequestParam(name = "id") int id) {
+		return new Cliente(id, "Maria", "1112233-00");
 	}
 }
